004b8de23e0dec76296d5c0ba4a84e545e0a142f
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-05-17 19:30:03 +0530

    assignment 1

--- DSA Lab/Code/Assignment_1.cpp
+++ DSA Lab/Code/Assignment_1.cpp
@@ -1,133 +1,119 @@
-/*
-A book consists of chapters, chapters consist of sections and sections consist
-of subsections. Construct a tree and print the nodes. Find the time and space
-requirements of your method.
-*/
-
 #include <iostream>
 using namespace std;
-class node 
-{
-    public:
+
+class node{
+    public: 
         char label[20];
         int ch_count;
-        node *child[10];
+        node *child[10];    
 }*root;
 
-class BST 
-{
-    public:
-    void create();
-    void display(node *r1);
-    BST()
-    {
-        root=NULL;
-    }
+class BST{
+	public:
+        void create();
+        void display(node *r);
+        BST()
+        {
+        root = NULL;
+        }
+
 };
-void BST::create()
-{
-    int i ,j, k, tchapters, tbook;
-    root=new node;
-    cout<<"Enter the name of the book "<<endl;
-    cin>>root->label;
-    cout<<"Enter the number of chapters the book contains ";
-    cin>>tchapters;
-    root->ch_count=tchapters;
-    for(i=0; i<tchapters; i++)
-    {
-        root->child[i]=new node;
-        cout<<"Enter the name of the chapter ";
-        cin>>root->child[i]->label;
-        
-        cout<<"Enter the number of sections in this chapter ";
-        cin>>root->child[i]->ch_count;
-        
-        for(j=0; j<root->child[i]->ch_count; j++)
-        {
-            root->child[i]->child[j]=new node;
-            cout<<"Enter section head ";
-            cin>>root->child[i]->child[j]->label;
-            
-            cout<<"Enter the number of sub-sections in this chapter ";
-            cin>>root->child[i]->child[j]->ch_count;
-            
-            for(k=0; k<root->child[i]->child[j]->ch_count; k++)
-            {
-                root->child[i]->child[j]->child[k]=new node;
-                cout<<"Enter sub-section head ";
-                cin>>root->child[i]->child[j]->child[k]->label;
-            }
-        }
-        
-    }
-}
-void BST::display(node *r1)
-{
-    int i,j,k,tchapters;
-    if (r1!=NULL)
-    {
-        cout<<"\n  -----Book Hierarchy-----";
-        cout<<"\n BOOK TITLE: "<<r1->label;
-        tchapters=r1->ch_count;
-        for(i=0; i<tchapters; i++)
-        {
-            cout<<"\n----------------"<<endl;
-            cout<<"\n CHAPTER: "<<i+1<<". ";
-            cout<<r1->child[i]->label<<endl;
-            
-            
-            for(j=0; j<r1->child[i]->ch_count; j++)
-            {
-                cout<<"\n SECTION: ";
-                cout<<r1->child[i]->child[j]->label<<endl;
-                
-                
-                for(k=0; k<r1->child[i]->child[j]->ch_count; k++)
-                {
-                    cout<<"\n SUB-SECTION: ";
-                    cout<<r1->child[i]->child[j]->child[k]->label<<endl;
-                }
-                
-            }
-            
-            
-        }
-    }
+
+void BST::create(){
+	root = new node;
+	cout<<"Enter the name of the book: "<<endl;
+	cin>>root->label;
+	cout<<"Enter the total number of chapters in the book: "<<endl;
+	int total;
+	cin>>total;
+	root->ch_count = total;
+	for(int i = 0; i<root->ch_count; i++){
+
+		root->child[i] = new node;
+		cout<<"Enter the name of the chapter"<<endl;
+		cin>>root->child[i]->label;
+		
+		cout<<"Enter the number of sections in the chapter"<<endl;
+		cin>>root->child[i]->ch_count;
+
+		for(int j = 0; j<root->child[i]->ch_count; j++){
+		
+			root->child[i]->child[j] = new node;
+			cout<<"Enter the name of the section"<<endl;
+			cin>>root->child[i]->child[j]->label;
+
+			cout<<"Enter the number of sub sections in the chapter"<<endl;
+			cin>>root->child[i]->child[j]->ch_count;
+
+			for(int k = 0; k<root->child[i]->child[j]->ch_count; k++){
+
+				root->child[i]->child[j]->child[k] = new node;
+				cout<<"Enter sub-section head"<<endl;
+				cin>>root->child[i]->child[j]->child[k]->label;
+			}
+
+		
+		}
+
+	}
+
 }
 
+void BST::display(node *r){
+	
+	if(r!=NULL){
+		
+		cout<<"Book Hierarchy"<<endl;
+		cout<<"Book Title: "<<r->label<<endl;
+		
+		for(int i = 0; i<r->ch_count; i++){
+		
+			cout<<" \n Chapter: "<<r->child[i]->label;
+			
+			for(int j = 0; j<r->child[i]->ch_count; j++){
+			
+				cout<<" \n Section: "<<r->child[i]->child[j]->label;
 
-int main()
-{
-    int choice  ;
-    BST bst;
-    while(1)
-    {
-        cout<<"\n\n--------------"<<endl;
-        cout<<"Book tree creation"<<endl;
-        cout<<"---------------"<<endl;
-        cout<<"1.Create "<<endl;
-        cout<<"2.Display "<<endl;
-        cout<<"3.Quit "<<endl;
-        cout<<"Enter your choice: ";
-        cin>>choice;
-        switch(choice )
-        {
-            case 1: 
-            bst.create();
-            
-            case 2:
-            bst.display(root);
-            break;
-            
-            case 3: 
-            exit(1);
-            
-            default:
-            cout<<"Wrong choice "<<endl;
-        }
-        
-    }
-    
 
-    return 0;
+				for(int k = 0; k<r->child[i]->child[j]->ch_count; k++){
+			
+					cout<<"\n Sub-section: "<<r->child[i]->child[j]->child[k]->label;
+
+				}
+			}	
+
+
+		}
+	
+	}
+
+}
+
+
+
+int main(){
+	int choice;
+	BST bst;
+
+	while(1){
+		cout<<"Book tree creation"<<endl;
+		cout<<"1. Create" <<endl;
+		cout<<"2. Display" <<endl;
+		cout<<"3. Quit"<<endl;
+		cin>>choice;
+		switch(choice){
+		
+			case 1: bst.create();
+			
+			case 2: bst.display(root);
+
+			case 3: exit(1);
+
+			default: cout<<"Wrong Choice"<<endl;
+
+		}
+
+	}
+
+ return 0;
 }
