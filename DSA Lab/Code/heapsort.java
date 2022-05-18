01137982fb4c7a496774674f6438dd3ef82d4045
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-05-19 03:42:12 +0530

    ass9 done

--- DSA Lab/Code/heapsort.java
+++ DSA Lab/Code/heapsort.java
@@ -1,81 +1,106 @@
-/*Implement heap sort in JAVA*/
+/*
+    heap : a complete binary tree that satisfies heap's property
+    max heap's property: the children nodes are smaller than root
+    min heap's property: the children nodes are greater than root
+    in the last level of complete binary tree, preference must be given to left side
+    heap is created to represent it as array
+    if the current node's index is i, then to calculate:
+        parent's index = (i/2)
+        left child = 2 * i + 1
+        right child = 2 * i + 2
+        height of the tree is logn
 
-import java.util.*;
- 
+    Time complexity of heapify is O(Logn). 
+    Time complexity of createAndBuildHeap() is O(n) 
+    And, hence the overall time complexity of Heap Sort is O(nLogn).
+    1. Convert unsorted array into max heap using heapify function
+
+*/
+
+import java.util.Scanner;
+
+
 public class heapsort {
-    public int[] heap;
-    public int count;
- 
-    public void downadjust(int i) {
-        int j, temp, n;
-        n = heap[0];
-        if (2 * i <= n) {
-            j = 2 * i;// j on left child of i
-            if (j + 1 <= n && heap[j + 1] > heap[j]) // j points to larger of left and right child
-            	j = j + 1;
-            if (heap[i] < heap[j]) {
-            	temp = heap[i];
-            	heap[i] = heap[j];
-            	heap[j] = temp;
-            	downadjust(j);
-            }
+	
+	void heapify(int arr[], int n, int i)
+	{
+		int largest = i; 
+		int left = 2 * i + 1; 
+		int right = 2 * i + 2; 
+
+		// If left child is larger than root
+		if (left < n && arr[left] > arr[largest])
+			largest = left;
+
+		// If right child is larger than largest so far
+		if (right < n && arr[right] > arr[largest])
+			largest = right;
+
+		// If largest is not root
+		if (largest != i) {
+			int swap = arr[i];
+			arr[i] = arr[largest];
+			arr[largest] = swap;
+
+			// Recursively heapify the affected sub-tree
+			heapify(arr, n, largest);
+		}
+	}
+
+    public void sort(int arr[])
+	{
+		int n = arr.length;
+
+		// Build heap (rearrange array)
+		for (int i = n / 2 - 1; i >= 0; i--)
+			heapify(arr, n, i);
+
+		// One by one extract an element from heap
+		for (int i = n - 1; i > 0; i--) {
+
+			// Move current root to end
+			int temp = arr[0];
+			arr[0] = arr[i];
+			arr[i] = temp;
+
+			// call max heapify on the reduced heap
+			heapify(arr, i, 0);
+		}
+	}
+
+    void printArray(int arr[])
+    {
+        for (int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
         }
-	}
- 
-    public void upadjust(int i) {
-        int temp;
-        while (i > 1 && heap[i] > heap[i / 2]) {
-            temp = heap[i];
-            heap[i] = heap[i / 2];
-            heap[i / 2] = temp;
-            i = i / 2;
+        System.out.println();
+    }
+
+	public static void main(String args[])
+	{
+
+		heapsort h = new heapsort();
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("MENU");
+        System.out.println("1. Input ");
+        System.out.println("2. Sort ");
+        System.out.println("3. Print");
+
+        System.out.println("Enter the number of elements: ");
+        int no = sc.nextInt();
+        int heap[] = new int[no];
+
+        for (int i = 0; i < no; i++) {
+            System.out.print("Enter element: ");
+                heap[i] = sc.nextInt();
         }
-	}
- 
-    public void insert(int x) {
-        heap[++heap[0]] = x;
-        upadjust(heap[0]);
-	}
- 
-    public void create() {
-        int i, x, n;
-        heap = new int[30];
-        heap[0] = 0;
-        Scanner reader = new Scanner(System.in);
-        System.out.println("\nEnter No. of elements : ");
-        n = reader.nextInt();
-        count = n;
-        System.out.println("\nEnter heap data : ");
-        for (i = 0; i < n; i++) {
-            x = reader.nextInt();
-            insert(x);
-        }
-	}
- 
-    public void sort() {
-        int last, temp;
-        while (heap[0] > 1) {
-            last = heap[0];
-            temp = heap[1];
-            heap[1] = heap[last];
-            heap[last] = temp;
-            heap[0]--;
-            downadjust(1);
-        }
-	}
- 
-    public void print() {
-        int n, i;
-	    n = count;
-        System.out.println("\nsorted data : ");
-        for (i = 1; i <= n; i++)
-            System.out.print(" " + heap[i]);
-	}
- 
-    public static void main(String[] args) {
-        heapsort myobject = new heapsort();
-        myobject.create();
-        myobject.sort();
-        myobject.print();
-	}
+
+        h.sort(heap);
+
+        System.out.println("Heap sorted array: ");
+        h.printArray(heap);
+
+        
+    }
 }
