d5709f5eb99fb62545083874b3f096726cccacde
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-05-18 23:27:44 +0530

    ass7

--- DSA Lab/Code/Assignment_7.cpp
+++ DSA Lab/Code/Assignment_7.cpp
@@ -1,191 +1,95 @@
-//Represent given graph using adjacency matrix / list to perform DFS adjacency list to
-//perform BFS. Use map of area around the college as a graph. 
-//Identify prominent landmark as nodes and perform DFS and BFS on that
-
-#include <iostream>
-#include <iomanip>
-#include <queue>
+#include<iostream>
+#include<string>
+#include<queue>
 using namespace std;
-
-class dfs
+int admj[50][50]={0,0};
+int visited[50]={0};
+void dfs(int s, int v, string arr[])
 {
-public:
-    int stack[50];
-    int top = -1;
-    int array[50][50] = {
-        
-    };
-    string name[10];
-    int city1, city2;
+	visited[s]=true;
+	cout<<arr[s]<<" ";
+	for (int i=0; i<v; i++)
+	{
+		if (admj[s][i] && !visited[i] )
+		{
+			dfs(i,v,arr);
+		}
+	}
+}
+void bfs(int s, int v, string arr[])
+{
+	bool visited[v];
+	for (int i=0; i<v; i++)
+	{
+		visited[i]=false;
+	}
+	int v2;
+	queue<int>bfsq;
+	if(!visited[s])
+	{
+		cout<<arr[s]<<" ";
+		visited[s]=true;
+		bfsq.push(s);
+		while(!bfsq.empty())
+		{
+			v2=bfsq.front();
+			for (int i=0; i<v; i++)
+			{
+				if (admj[v2][i] && !visited[i] )
+				{
+					cout<<arr[i]<<" ";
+					visited[i]=true;
+					bfsq.push(i);
+				}
+			}
+			bfsq.pop();
+		}
+	}
+}
 
-    int poped;
-    int visited[30];
-
-    int bfs_visited[30];
-    int q[50];
-    int front, rear;
-
-    dfs()
-    {
-        for (int i = 0; i < 30; i++)
-        {
-            visited[i] = 0;
-            bfs_visited[i] = 0;
-        }
-    }
-
-    void push(int a)
-    {
-        stack[++top] = a;
-    }
-
-    int pop()
-    {
-        int Value;
-        Value = stack[top];
-        top--;
-        return Value;
-    }
-
-    void array_creation(int cities)
-    {
-        int array[cities][cities];
-        for (int i = 0; i < cities; i++)
-        {
-            for (int j = 0; j < cities; j++)
-            {
-                array[i][j] = 0;
-            }
-        }
-    }
-    
-    void insert(int city_a, int city_b)
-    {
-
-        array[city_a][city_b] = 1;
-        array[city_b][city_a] = array[city_a][city_b];
-    }
-
-    void DisplayGraph(int cities)
-    {
-        cout << setw(5) << "";
-        for (int i = 0; i < cities; i++)
-        {
-            cout << setw(5) << name[i];
-        }
-        cout << endl;
-        for (int i = 0; i < cities; i++)
-        {
-            cout << setw(5) << name[i];
-            for (int j = 0; j < cities; j++)
-            {
-                cout << setw(5) << array[i][j];
-            }
-            cout << endl;
-        }
-    }
-    int displayDfs(int v1,int cities)
-    {   int v2;
-        cout << "DFS search is" << endl;
-        push(v1);
-        while (top != -1)
-        {
-            v1 = pop();
-            if (visited[v1] == 0)
-            {
-                cout << v1 << endl;
-                visited[v1] = 1;
-            }
-            for (int v2 = 0; v2 <cities; v2++)
-                if (array[v1][v2] == 1 && visited[v2] == 0)
-                    push(v2);
-        }
-        return 0;
-    }
-    int displayBfs(int v1,int cities)
-    {
-        cout << "BFS search is \n";
-
-        int v2;
-        bfs_visited[v1] = 1;
-        front = rear = -1;
-        q[++rear] = v1;
-        while (front != rear)
-        {
-            v1 = q[++front];
-            cout 
-                 << v1<< "\n";
-            for (int v2 = 0; v2 < cities; v2++)
-            {
-                if (array[v1][v2] == 1 && bfs_visited[v2] == 0)
-                {
-                    q[++rear] = v2;
-                    bfs_visited[v2] = 1;
-                }
-            }
-        }
-        return 0;
-    }
-};
-
+			
 int main()
 {
-    dfs obj;
-    int cities, city_a, city_b, edge, choice, v2;
-
-    do
-    {
-        cout << "==============Enter your choice==============" << endl;
-        cout << "1) Insert" << endl;
-        cout << "2) Display By DFS" << endl;
-        cout << "3) Display by BFS" << endl;
-        cout << "4) Exit" << endl;
-        cin >> choice;
-
-        switch (choice)
-        {
-        case 1:
-            cout << "Enter how many cities:- ";
-            cin >> cities;
-            obj.array_creation(cities);
-
-            for (int i = 0; i < cities; i++)
-            {
-                cout << "Enter the name of city ";
-                cin >> obj.name[i];
-            }
-
-            cout << "\nEnter number of edge ";
-            cin >> edge;
-            for (int i = 0; i < edge; i++)
-            {
-                cout << "\nEnter the city number 1:- ";
-                cin >> city_a;
-                cout << "\nEnter the city number 2:- ";
-                cin >> city_b;
-                obj.insert(city_a, city_b);
-            }
-            obj.DisplayGraph(cities);
-            break;
-
-        case 2:
-            cout << "Enter the vertex from which you want to traverse: ";
-            cin >> v2;
-            obj.displayDfs(v2,cities);
-            break;
-
-        case 3:
-            cout << "Enter the vertex from which you want to traverse: ";
-            cin >> v2;
-            obj.displayBfs(v2,cities);
-            break;
-        case 4:
-            break;
-        default:
-            cout << "Invalid Choice" << endl;
-            break;
-        }
-    } while (choice < 4);
-
+	int v1,c;
+	cout<<"Enter the number of vertices :";cin>>v1;
+	string city[v1];
+	for (int i=0; i<v1; i++)
+	{
+		cout<<"Enter the name of the cities :"<<i<<" ";cin>>city[i];
+	}
+	cout<<"Enter the distance between cities :";
+	for (int i=0; i<v1; i++)
+	{
+		for (int j=i+1; j<v1; j++)
+		{
+			cout<<"\nEnter the distance between "<<city[i]<<" and "<<city[j]; cin>>c;
+			admj[i][j]=admj[j][i]=c;
+		}
+	}
+	
+	cout<<endl;
+	for (int i=0; i<v1; i++)
+	{
+		cout<<"\t"<<city[i];
+	}
+	cout<<endl;
+	for (int i=0; i<v1; i++)
+	{
+		cout<<city[i]<<"\t";
+		for (int j=0; j<v1; j++)
+		{
+			cout<<admj[i][j]<<"\t";
+		}
+		cout<<endl;
+	}
+	
+	cout<<"Enter the starting vertex by writing the index number of the city";
+	int n; cin>>n;
+	cout<<"DFS" <<endl;
+	dfs(n,v1,city);
+	cout<<endl;
+	cout<<"BFS"<<endl;
+	bfs(n,v1,city);
     return 0;
 }
+	
