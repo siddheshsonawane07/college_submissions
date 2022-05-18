89d6d1c45e71dea35ae41f1a627f768361441b17
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-05-18 17:25:34 +0530

    ass5

--- DSA Lab/Code/Assignment_5.cpp
+++ DSA Lab/Code/Assignment_5.cpp
@@ -3,6 +3,7 @@ Consider Telephone book database of N clients. Make use of hash table
 implementation to quickly look up clients telephone number. Make use of
 two collision handling techniques and compare them using number of
 comparisons required to find a set of telephone numbers
+unsigned long long = 64 bits
 
 
 */
@@ -10,8 +11,6 @@ comparisons required to find a set of telephone numbers
 
 #include<iostream>
 #include<cstring>
-
-//int mod = 1e9+7;
 
 using namespace std;
 
@@ -34,18 +33,47 @@ public:
 
 	}
 
+	
+	int hash_function(unsigned long long key) {
+		return key % 10;
+	}
+
+	void hash_collision_linear_probing(unsigned long long mobile[], string name[], unsigned long long key) {
+		int adr = hash_function(key);
+		while (mobile[(adr % 10)] != 0) {
+			adr++;
+		}
+
+		mobile[adr % 10] = key;
+		cout << "Enter name of the person :- ";
+		cin >> name[adr % 10];
+		cout << endl;
+	}
+
+	void hash_collision_quadratic_probing(unsigned long long mobile[], string name[], unsigned long long key) {
+		int adr = hash_function(key);
+		int i = 1;
+		while (mobile[(adr % 10)] != 0) {
+			adr += (i * i);
+			i++;
+		}
+
+		mobile[adr % 10] = key;
+		cout << "Enter name of the person :- ";
+		cin >> name[adr % 10];
+		cout << endl;
+	}
+
 	void insert_record() {
 
 		cout << "Enter no of records you want to enter :- ";
 		cin >> num;
 		cout << endl;
-		cout << "Which Collision handling technique do you want to use"
-			<< endl << "1. Linear Probing" << endl << "2. Quadratic Probing" << endl;
+		cout << "Which Collision handling technique do you want to use" << endl << "1. Linear Probing" << endl << "2. Quadratic Probing" << endl;
 		int flag;
 		cout << "Enter your Choice :- ";
 		cin >> flag;
 		cout << endl;
-
 
 		while (num--) {
 			cout << "Enter Telephone no :- ";
@@ -71,33 +99,6 @@ public:
 		}
 	}
 
-	void hash_collision_linear_probing(unsigned long long mobile[], string name[], unsigned long long key) {
-		int adr = hash_function(key);
-
-		while (mobile[(adr % 10)] != 0) {
-			adr++;
-		}
-
-		mobile[adr % 10] = key;
-		cout << "Enter name of the person :- ";
-		cin >> name[adr % 10];
-		cout << endl;
-	}
-
-	void hash_collision_quadratic_probing(unsigned long long mobile[], string name[], unsigned long long key) {
-		int adr = hash_function(key);
-		int i = 1;
-		while (mobile[(adr % 10)] != 0) {
-			adr += (i * i);
-			i++;
-		}
-
-		mobile[adr % 10] = key;
-		cout << "Enter name of the person :- ";
-		cin >> name[adr % 10];
-		cout << endl;
-	}
-
 	void display() {
 		cout << "Index\tName\tMobile" << endl;
 		for (int i = 0; i < 10; i++) {
@@ -105,9 +106,6 @@ public:
 		}
 	}
 
-	int hash_function(unsigned long long key) {
-		return key % 10;
-	}
 
 };
 
